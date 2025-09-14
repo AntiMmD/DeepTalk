d0abbc1c89f5376f22a48fa51525203913a22cef
IcePack <mhmd.zayeri@gmail.com>
2025-09-14 18:17:33 +0330

    User can submit a post and save it to the database

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from posts.models import Post
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -19,4 +19,27 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST"')
         self.assertContains(response, '<input name="header_input"')
         self.assertContains(response, '<input name="body_input"')
+    
+    def test_can_submit_the_post_form(self):
+
+        header = 'header test'
+        body = 'body test'
+        response = self.client.post('/posts/new',
+                            data={'header_input': header, 'body_input': body})
         
+        
+        self.assertEqual(Post.objects.count(), 1, msg="Post object was not created. Maybe define the Post model fields?")
+        
+        post_obj = Post.objects.last()  
+        self.assertEqual(post_obj.header, header)
+        self.assertEqual(post_obj.body, body)
+
+        redirected_url = f'/posts/posted/{post_obj.id}'
+        self.assertRedirects(response, redirected_url)
+
+        response = self.client.get(redirected_url)
+        self.assertTemplateUsed(response, 'posts/postView.html')
+        self.assertEqual(response.context['header'], header)
+        self.assertEqual(response.context['body'], body )
+        self.assertContains(response, '<p id="posted_header"')
+        self.assertContains(response, '<p id="posted_body"')
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Post
+from django.shortcuts import redirect
+
 def home_page(request):
     return render(request, 'posts/home.html')
 
 def post_form(request):
+    if request.method == 'POST':
+        post_header = request.POST['header_input']
+        post_body = request.POST['body_input']
+        post_obj = Post.objects.create(header= post_header, body= post_body)
+        return redirect(f'/posts/posted/{post_obj.id}')
+    
     return render(request, 'posts/postForm.html')
+
+def post_view(request, id):
+    post_obj= Post.objects.get(id=id)
+    post_header = post_obj.header
+    post_body = post_obj.body
+    return render(request, 'posts/postView.html', context={'header':post_header, 'body':post_body})
