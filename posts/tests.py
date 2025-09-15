44cc63f11dd9c0d1a9e4ace0e7999bfc8caa7123
IcePack <mhmd.zayeri@gmail.com>
2025-09-15 20:30:36 +0330

    Refactor tests to use reverse instead of url hard-coding

--- posts/tests.py
+++ posts/tests.py
@@ -1,20 +1,21 @@
 from django.test import TestCase
 from posts.models import Post
+from django.urls import reverse
 # Create your tests here.
 
 class HomePageTest(TestCase):
     
     def test_home_page_uses_home_template(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'posts/home.html')
 
     def test_home_page_post_button(self):
-        response = self.client.get('/')
-        self.assertContains(response, '<a href="/posts/new">')
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, f'<a href="{reverse("post_form")}">')
         self.assertContains(response, '<button id="create_post_button">')  
 
     def test_create_post_button_renders_a_form_template_correctly(self):
-        response = self.client.get('/posts/new')
+        response = self.client.get(reverse('post_form'))
         self.assertTemplateUsed(response, 'posts/postForm.html')
         self.assertContains(response, '<form method="POST"')
         self.assertContains(response, '<input name="header_input"')
@@ -24,7 +25,7 @@ class HomePageTest(TestCase):
 
         header = 'header test'
         body = 'body test'
-        response = self.client.post('/posts/new',
+        response = self.client.post(reverse('post_form'),
                             data={'header_input': header, 'body_input': body})
         
         
@@ -34,7 +35,7 @@ class HomePageTest(TestCase):
         self.assertEqual(post_obj.header, header)
         self.assertEqual(post_obj.body, body)
 
-        redirected_url = f'/posts/posted/{post_obj.id}'
+        redirected_url = f'{reverse("post_view", args=[post_obj.id])}'
         self.assertRedirects(response, redirected_url)
 
         response = self.client.get(redirected_url)
