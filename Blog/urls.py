42d49164fe076f3c18f320dd278484684d291096
IcePack <mhmd.zayeri@gmail.com>
2025-09-15 20:27:21 +0330

    Fix a typo, from name='post_from' to 'post_form'

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,7 +5,7 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home'),
-    path('posts/new', views.post_form, name='post_from'),
+    path('posts/new', views.post_form, name='post_form'),
     path('posts/posted/<int:id>', views.post_view, name= 'post_view'),
 ]
 
